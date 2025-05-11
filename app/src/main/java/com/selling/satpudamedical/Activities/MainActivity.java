4e4b2a6e4d03966ad83c5bc2f0d506fc8e932923
roshanghogale <roshanghogale9@gmail.com>
2025-05-11 08:06:06 +0530

    initial commit

---
+++ app/src/main/java/com/selling/satpudamedical/Activities/MainActivity.java
@@ -0,0 +1,64 @@
+package com.selling.satpudamedical.Activities;
+
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.selling.satpudamedical.Fragments.ChatFragment;
+import com.selling.satpudamedical.Fragments.HomeFragment;
+import com.selling.satpudamedical.Fragments.InfoFragment;
+import com.selling.satpudamedical.Fragments.ProfileFragment;
+import com.selling.satpudamedical.Fragments.VideosFragment;
+import com.selling.satpudamedical.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_home) {
+                selectedFragment = new HomeFragment();
+            } else if (itemId == R.id.nav_chat) {
+                selectedFragment = new ChatFragment();
+            } else if (itemId == R.id.nav_info) {
+                selectedFragment = new InfoFragment();
+            } else if (itemId == R.id.nav_videos) {
+                selectedFragment = new VideosFragment();
+            } else if (itemId == R.id.nav_profile) {
+                selectedFragment = new ProfileFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+            return true;
+        });
+
+        // Set default fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new HomeFragment())
+                    .commit();
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
