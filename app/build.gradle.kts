bc53802bcb7fea17d8670706383e54ae9234d2c3
narendra2074 <narendratayde2074@gmail.com>
2025-05-15 11:32:44 +0530

    full commmit from narendra tayade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -29,6 +30,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
 
 dependencies {
@@ -37,7 +41,12 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.core.ktx)
     testImplementation(libs.junit)
+    implementation(libs.exoplayer)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-}
+    implementation(libs.circularstatusview)
+    implementation(libs.whynotimagecarousel)
+    implementation(libs.circleimageview)
+    implementation(libs.glide) }
